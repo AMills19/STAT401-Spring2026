15d36c8b38b1090cd769955c0fcfb42deae52cac
AMills19 <144487240+AMills19@users.noreply.github.com>
2026-01-26 14:46:54 -0500

    Add files via upload

--- T-Test.R
+++ T-Test.R
@@ -10,6 +10,25 @@ lines (density(x), col='green')
 
 lines (density (y), col='blue')
 
+#Add title to this plot, title name should be "T-Test"
+pts = seq(-4.5, 4.5, length=100)
+
+ plot(pts,dt(pts, df=9),col='red',type='l',main="T-test")
+ 
+ lines (density(x), col='green')
+
+ lines (density (y), col='blue')
+
+#Add labels to X and Y axis, and label names should be, for X add "Sequence", Y add "Density of X and Y"
+pts = seq(-4.5, 4.5, length=100)
+ 
+ plot(pts,dt(pts, df=9),col='red',type='l',main="T-test", xlab="Sequence", ylab="Density of X and Y")
+
+lines (density(x), col='green')
+
+ lines (density (y), col='blue')
+
+
 #Apply the function for T-test
 ttest = t.test (x,y)
 ttest
@@ -25,6 +44,12 @@ sample estimates:
  mean of x  mean of y 
  0.1445300 -0.8364533 
 
+#Create a dashed line insted of a continous line
+pts = seq(-4.5, 4.5, length=100)
+ 
+ plot(pts,dt(pts, df=9),col='red',type='l',main="T-test", xlab="Sequence", ylab="Density of X and Y", lty=2)
 
+lines (density(x), col='green')
 
+ lines (density (y), col='blue')
 
